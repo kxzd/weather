648adbef4fe65bc08c6eee3afc8e7a337f15b35d
kxzd <shiftyy_@hotmail.com>
2022-01-19 22:04:54 +1100

    Fixed links

--- src/index.js
+++ src/index.js
@@ -68,21 +68,21 @@ function getWeatherImage(data) {
     const weather = data.weather[0].main;
     switch (weather) {
         case "Clear":
-            weatherImage.src = "../dist/img/clear.svg";
+            weatherImage.src = "img/clear.svg";
             break;
         case "Clouds":
-            weatherImage.src = "../dist/img/cloudy.svg";
+            weatherImage.src = "img/cloudy.svg";
             break;
         case "Rain":
         case "Drizzle":
         case "Mist":
-            weatherImage.src = "../dist/img/rain.svg";
+            weatherImage.src = "img/rain.svg";
             break;
         case "Thunderstorm":
-            weatherImage.src = "../dist/img/thunder.svg";
+            weatherImage.src = "img/thunder.svg";
             break;
         case "Snow":
-            weatherImage.src = "../dist/img/snow.svg";
+            weatherImage.src = "img/snow.svg";
             break;
         default: 
             weatherImage.src = ""
